3ab9995d8295531dda37b149192003918d5e2a2a
cushkia1 <cushkia1@gmail.com>
2026-03-10 10:35:24 -0400

    Updates to HUcalc to prevent SI inputs outside 0-1

--- R/HUcalc.R
+++ R/HUcalc.R
@@ -3,8 +3,8 @@
 #' \code{HUcalc} computes habitat units given a set of suitability indices,
 #'  a habitat suitability index equation, and habitat quantity.
 #'
-#' @param SI.out is a vector of application-specific suitability indices,
-#'   which can be produced from SIcalc.
+#' @param SI.out is a vector of application-specific suitability indices between
+#'   0 and 1, which can be produced from SIcalc.
 #' @param habitat.quantity is a numeric of habitat size associated with these
 #'   suitability indices (i.e., length, area, or volume).
 #' @param HSIfunc is a function for combination of the suitability indices.
@@ -41,12 +41,16 @@ HUcalc <- function(SI.out, habitat.quantity, HSIfunc,...){
   # Create an empty vector to store outputs
   HU.out <- as.data.frame(matrix(NA,nrow=1,ncol=3))
   colnames(HU.out) <- c("Quality", "Quantity", "IndexUnits")
-
-  # Compute outputs
-  HU.out$Quality <- HSIfunc(SI.out,...)
-  HU.out$Quantity <- habitat.quantity
-  HU.out$IndexUnits <- HU.out$Quality * HU.out$Quantity
-
+  
+  if (any(SI.out < 0 | SI.out > 1, na.rm = TRUE)) {
+    HU.out <- "SI.out inputs not within 0 to 1 range."
+  } else {
+    # Compute outputs
+    HU.out$Quality <- HSIfunc(SI.out,...)
+    HU.out$Quantity <- habitat.quantity
+    HU.out$IndexUnits <- HU.out$Quality * HU.out$Quantity
+  }
+  
   # Return habitat summary
   return(HU.out)
 }
